bc27a93440538b31d2e501d07c1da0d8e15495e2
Deepak Sastry <deepak_sastry@intuit.com>
2018-08-29 19:49:36 +0530

    Adding the support for data provider and parallel execution

--- src/test/java/com/autothan/test/ParseWikiPage.java
+++ src/test/java/com/autothan/test/ParseWikiPage.java
@@ -5,6 +5,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.client.Invocation;
@@ -18,39 +19,54 @@ import static org.testng.Assert.assertEquals;
  */
 public class ParseWikiPage extends TestHelper{
 
-    @Test()
-    public void testParsePageContent() throws Exception
+    @DataProvider
+    public static Object[][] wikiLinks() {
+        Object[][] result = new Object[][]{
+                {1,"https://en.wikipedia.org/wiki/The_invalidmovie"},
+                {2,"https://en.wikipedia.org/wiki/Goodfellas"},
+                {3,"https://en.wikipedia.org/wiki/The_Shawshank_Redemption"},
+                {4,"https://en.wikipedia.org/wiki/Seven_(1995_film)"},
+                {5,"https://en.wikipedia.org/wiki/Inception"},
+                {6,"https://en.wikipedia.org/wiki/The_Godfather"},
+
+
+        };
+        return result;
+    }
+
+    @Test(dataProvider = "wikiLinks",singleThreaded = false,threadPoolSize = 5)
+    public void testParsePageContent(Integer index,String wikiURL) throws Exception
     {
         String imdbURL = null;
-        String wikiDirectorName = null;
-        Invocation.Builder wikiPageBuilder = createBuilder("https://en.wikipedia.org/wiki/The_Godfather");
+        String movieDirector = null;
+        Invocation.Builder wikiPageBuilder = createBuilder(wikiURL);
         wikiPageBuilder.header("Accept", MediaType.APPLICATION_JSON_TYPE);
         Response response = wikiPageBuilder.get();
-        assertEquals(response.getStatus(),200);
-
+        if(index.equals(1)){
+            assertEquals(response.getStatus(),404);
+            return;
+        }else
+        {
+            assertEquals(response.getStatus(),200);
+        }
         Document doc = Jsoup.parse(response.readEntity(String.class), "UTF-8");
         Elements wikiElements = doc.select("a");
 
-        //Find the IMDB URL
+        //Find the IMDB URL & Director Name from the wikipedia page
         for(Element element : wikiElements){
             //System.out.println(element.absUrl("href"));
             if(element.absUrl("href").contains("imdb")) {
                 imdbURL = element.absUrl("href");
-                break;
             }
-        }
 
-        //Find the Director name
-        for(Element directors : wikiElements)
-        {
-            if(String.valueOf(directors.getElementsContainingText("Films directed by")).length() >0){
-                wikiDirectorName =  StringUtils.substringAfter(String.valueOf(directors.getElementsContainingText("Films directed by")),">Films directed by").replace("</a>","");
-                break;
+            if(String.valueOf(element.getElementsContainingText("Films directed by")).length() >0){
+                movieDirector =  StringUtils.substringAfter(String.valueOf(element.getElementsContainingText("Films directed by")),">Films directed by").replace("</a>","");
             }
         }
 
         System.out.println("IMDB URL is::" + imdbURL);
-        System.out.println("Director :: " + wikiDirectorName) ;
+        System.out.println("Director :: " + movieDirector) ;
+        //System.out.println("Output :: " + response.readEntity(String.class));
 
         //Request to IMDB Page
         Invocation.Builder imdbBuilder = createBuilder(imdbURL);
@@ -64,9 +80,8 @@ public class ParseWikiPage extends TestHelper{
 
         //Compare the Director names obatained from WIKI and IMDB
         for(Element element : imdbElements){
-            if(String.valueOf(element.getElementsByAttributeValue("title",wikiDirectorName)).length()>0) {
-                String imdbDirectorName=element.getElementsByAttributeValue("title",wikiDirectorName).get(0).childNode(0).toString().trim();
-                assertEquals(imdbDirectorName,wikiDirectorName.trim(),"Director Assertion Passed");
+            if(String.valueOf(element.getElementsByAttributeValue("title",movieDirector)).length()>0) {
+                assertEquals(element.getElementsByAttributeValue("title",movieDirector).get(0).childNode(0).toString().trim(),movieDirector.trim(),"Director Assertion Passed");
                 break;
             }
 
@@ -76,3 +91,4 @@ public class ParseWikiPage extends TestHelper{
 
 
 }
+
